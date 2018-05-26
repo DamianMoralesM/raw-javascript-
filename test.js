54dfb1dab3e4e69dc33bdbd9674892169ea66259
unknown <damian_morales_utn@hotmail.com>
2018-05-25 23:05:53 -0300

    changes in render image

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
+/*
+The images placed in the column to be displayed have ids like imagen1, imagen2 ... imagen45
+When one of that images is clicked the function #render() is fired to displayed the one which was clicked
+on one of the three columns place1,place2 or place3 
+
+The logic is repited again when onother image is clicked.
+
+The image data is the same for de column and  place1,place2 and place3
+but we have two diferents ids , id for the one in placed in place1,place2 and place3
+and idProxy for the one in the column
+
+*/
+
+
 class Imagen {
 
 	constructor(id,idProxy,src,alt,style,classes,counter){
@@ -37,6 +51,19 @@ TodoBtn.addEventListener('click', function(){
 	lista.removeChild(item);
 	
 }
+
+function createImage(objetoImagen){
+
+  var image = document.createElement('img');
+    image.id=objetoImagen.id;
+    image.src=objetoImagen.src;
+    image.alt=objetoImagen.alt;
+    image.setAttribute("onclick","count;");
+
+    return image;
+
+
+ }
 		
 /*	
 fetch('data.json')
@@ -64,32 +91,32 @@ fetch('data.json')
   }
 
 
+
   function render(imagen){
-  
-  	
+     	
   	for (var i = 0 ; i <= imagesList.length - 1; i++) {
   		
   		 if (imagesList[i].idProxy == imagen.id){
-  			item = '<img '+ 'id="' +imagesList.id + '" src="' + imagesList[i].src + '" style="' + imagesList[i].style + '"" >'
-  			document.getElementById('place2').innerHTML = item; 
+  			item = createImage(imagesList[i]);
+
+  			 document.getElementById('place2').appendChild(item, document.getElementById('place2').firstChild); 
   		}
   		
   	} 
   }
 
   function inicializateElements(){
-  		img1 = new Imagen ("pic1","image1","imag_1.jpg","imagen","  width: 120px; height: 120px","",0);
-  		img2 = new Imagen ("pic2","image2","imag_2.jpg","imagen","  width: 120px; height: 120px","",0);
-  		img3 = new Imagen ("pic3","image3","imag_3.jpg","imagen","  width: 120px; height: 120px","",0);
+  		img1 = new Imagen ("pic1","image1","imag_1.jpg","imagen","  width: 320px; height: 120px","",0);
+  		img2 = new Imagen ("pic2","image2","imag_2.jpg","imagen","  width: 320px; height: 120px","",0);
+  		img3 = new Imagen ("pic3","image3","imag_3.jpg","imagen","  width: 320px; height: 120px","",0);
   		imagesList.push(img1);
   		imagesList.push(img2);
-		imagesList.push(img3);
+	   	imagesList.push(img3);
  }
 
 
 
- function makeFit(item, imagen){
-
-
- }
+ 
+ 
   inicializateElements();
+ 
